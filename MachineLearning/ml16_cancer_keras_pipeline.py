11288c95b97c07247ad0982eebadb88d4b387d7f
bitcamp <cdgus1514@gmail.com>
2019-08-07 20:12:39 +0900

    cdh

--- MachineLearning/ml16_cancer_keras_pipeline.py
+++ MachineLearning/ml16_cancer_keras_pipeline.py
@@ -8,8 +8,6 @@ from keras.layers import Dense, Dropout
 
 from sklearn.metrics import accuracy_score
 from sklearn.utils.testing import all_estimators
-import warnings
-warnings.filterwarnings("ignore")
 
 cancer = load_breast_cancer()   #분류
 
@@ -19,7 +17,6 @@ print(x.shape)  # (569, 30)
 print(y.shape)  # (569, )
 
 ## 데이터 분할
-warnings.filterwarnings("ignore")
 x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.2, train_size=0.8, shuffle=True)
 print("x_train shape >> ", x_train.shape)   # (455,30)
 print("y_train shape >> ", y_train.shape)   # (455,)
@@ -42,28 +39,33 @@ def bulid_model(keep_prob=0.5, optimizer="adam"):
     return model
 
 
-## 하이퍼파라미터
-def create_hyperparameters():
-    batches = [10,20,30,40,50]
-    optimizer = ['rmsprop', 'adam', 'adadelta']
-    dropout = np.linspace(0.1, 0.5, 5)
-    return{"batch_size":batches, "optimizer":optimizer, "keep_prob":dropout}
+# ## 하이퍼파라미터
+# def create_hyperparameters():
+#     batches = [10,20,30,40,50]
+#     optimizer = ['rmsprop', 'adam', 'adadelta']
+#     dropout = np.linspace(0.1, 0.5, 5)
+#     return{"batch_size":batches, "optimizer":optimizer, "keep_prob":dropout}
 
 
+prameter = {
+    "model__batch_size": [10,20,30,40,50],
+    "model__optimizer": ["adam", "adadelta", "rmsprop"],
+    "model__keep_prob": [0.1, 0.5, 5]
+}
 
 ## 튜닝
 from keras.wrappers.scikit_learn import KerasClassifier
 model = KerasClassifier(build_fn=bulid_model, verbose=1)
 
-hyperparameters = create_hyperparameters()
+# hyperparameters = create_hyperparameters()
 
 
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import MinMaxScaler
 
 pipe = Pipeline([("scaler", MinMaxScaler()), ("model", model)])
-pipe.fit(x_train, y_train)
-print("테스트 점수 >> ", pipe.score(x_test, y_test))
+# pipe.fit(x_train, y_train)
+# print("테스트 점수 >> ", pipe.score(x_test, y_test))
 
 
 
@@ -71,8 +73,12 @@ from sklearn.model_selection import RandomizedSearchCV
 from sklearn.model_selection import KFold
 
 kfold_cv = KFold(n_splits=5, shuffle=True)
-search = RandomizedSearchCV(pipe.get_params().keys(), hyperparameters, cv=kfold_cv)
+search = RandomizedSearchCV(pipe, prameter, cv=kfold_cv)
 
 search.fit(x_train, y_train)
 
 print(search.best_params_)
+
+from sklearn.metrics import accuracy_score
+y_pred = search.predict(x_test)
+print('>> 최종 정답률:', accuracy_score(y_test, y_pred))
