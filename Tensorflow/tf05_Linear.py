6b3dcacded967f1846674e6dbda48618860955bf
unknown <cdgus1514@gmail.com>
2019-08-19 09:05:11 +0900

    cdh

--- Tensorflow/tf05_Linear.py
+++ Tensorflow/tf05_Linear.py
@@ -2,8 +2,8 @@ import tensorflow as tf
 
 tf.set_random_seed(777)
 
-x_train = [1,2,3]
-y_train = [1,2,3]
+x_train = [1, 2, 3]
+y_train = [1, 2, 3]
 
 W = tf.Variable(tf.random_normal([1], name="weight"))
 b = tf.Variable(tf.random_normal([1], name="bias"))
@@ -16,7 +16,9 @@ hypothesis = x_train * W + b
 cost = tf.reduce_mean(tf.square(hypothesis - y_train))  # loss = "mse"
 
 # optimizer
-train = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(cost)    # optimizer="GradientDescent"
+train = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(
+    cost
+)  # optimizer="GradientDescent"
 
 
 # 실행
@@ -25,8 +27,8 @@ with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
 
     ## fit
-    for step in range(2001):    # epochs
-        _, cost_val, W_val, b_val = sess.run([train, cost, W, b])   # fit
+    for step in range(2001):  # epochs
+        _, cost_val, W_val, b_val = sess.run([train, cost, W, b])  # fit
 
         if step % 20 == 0:
             print(step, cost_val, W_val, b_val)
